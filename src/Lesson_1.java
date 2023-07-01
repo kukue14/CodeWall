d31bbbbda8c1e640bb1b26df0d579d626dad0ee6
kukue14 <kukue014@gmail.com>
2023-07-01 19:30:39 +0630

    1.7.2023

---
+++ src/Lesson_1.java
@@ -0,0 +1,10 @@
+public class Lesson_1 {
+    public static void main(String[] args) {
+        String name = "Thu Htet Naing";
+        System.out.println(name);
+        name = "Mya Mya";
+        System.out.println(name);
+        System.out.println(name);
+    }
+}
+
