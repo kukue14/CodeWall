1c7696d2013dfcc38afd66ce9029a3d4bb865d40
kukue14 <kukue014@gmail.com>
2023-07-09 12:15:12 +0630

    9.7.2023 Operators

---
+++ src/Chapter_1/Operator/Intro/ArithmeticOperator.java
@@ -0,0 +1,10 @@
+package Chapter_1.Operator.Intro;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+
+//        Operator Presidence
+        int result6 = 5 + 2 * 7 - 10 / 3 + 10 % 3;
+        System.out.println(result6);
+    }
+}
