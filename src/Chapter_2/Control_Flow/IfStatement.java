7d7dc64d598bb8a1724ef64d38f85aeaafa19180
kukue14 <kukue014@gmail.com>
2023-07-09 11:32:28 +0630

    9.7.2023 Control Flow

--- src/Chapter_2/Control_Flow/IfStatement.java
+++ src/Chapter_2/Control_Flow/IfStatement.java
@@ -1,2 +1,28 @@
-package Chapter_2.Control_Flow;public class IfStatement {
+package Chapter_2.Control_Flow;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        int a = 3;
+
+//        if
+        if (a > 0) {
+            System.out.println("a is greater than 0.");
+        }
+
+//        if else
+        if (a < 0) {
+            System.out.println("a is less than 0.");
+        } else {
+            System.out.println("a is greater than 0.");
+        }
+
+//        if else if
+        if (a > 0) {
+            System.out.println("a is greater than 0.");
+        } else if (a < 0) {
+            System.out.println("a is less than 0.");
+        } else {
+            System.out.println("a is 0.");
+        }
+    }
 }
