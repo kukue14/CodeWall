9373d0acc3fc07e9c09358d7410a376d06683f94
kukue14 <kukue14@gmail.com>
2023-07-28 21:23:43 +0630

    adding lessons

--- src/Chapter_2_ControlFlows/Lessons/PyramidStars.java
+++ src/Chapter_2_ControlFlows/Lessons/PyramidStars.java
@@ -20,20 +20,40 @@ public class PyramidStars {
             System.out.println();
         }
         FormatLite.Title("Reverse Pyramid Stars on Right");
-        for(int i = 0; i<5; i++){
-            for(int k=1; k<=i; k++)
+                for (int i = 0; i < 5; i++) {
+                    for (int k = 1; k <= i; k++)
+                        System.out.print("   ");
+                    for (int j = 5; j > i; j--)
+                        System.out.print(" * ");
+                    System.out.println();
+                }
+
+        FormatLite.Title("Pyramid Stars on Right");
+        for (int i = 0; i < 5; i++) {
+            for (int k = 4; k > i; k--)
                 System.out.print("   ");
-            for(int j = 5; j>i; j--)
+            for (int j = 0; j <= i; j++)
                 System.out.print(" * ");
             System.out.println();
         }
-        FormatLite.Title("Pyramid Stars on Right");
-        for(int i = 0; i<5; i++){
-            for(int k=4; k>i; k--)
-                System.out.print("   ");
-            for(int j = 0; j<=i; j++)
-                System.out.print(" * ");
+        FormatLite.Underline('=', 50);
+        FormatLite.Title("Using if");
+//        Using if
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (j >= 6 - i) System.out.print(" * ");
+                else System.out.print("   ");
+            }
+            System.out.println();
+        }
+        FormatLite.Underline('-', 50);
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (j >= i) System.out.print(" * ");
+                else System.out.print("   ");
+            }
             System.out.println();
         }
     }
 }
+
